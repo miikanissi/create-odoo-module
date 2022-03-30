8553dbcde3a324fde98e36e29daa375874503acc
Miika Nissi <miika@miikanissi.com>
2022-03-30 19:29:08 -0400

    [IMP] Add templates and create module dir

---
+++ templates/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "$name",
+    "version": "$version",
+    "category": "$category",
+    "summary": "$summary",
+    "description": "$description",
+    "website": "$website",
+    "author": "$author",
+    "license": "$license",
+    "application": False,
+    "installable": True,
+    "depends": [$depends],
+    "data": [$data],
+}
